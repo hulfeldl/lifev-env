a0810142f4eced74bcb8ef31d8a2fef208661de5
Simone Deparis <simone.deparis@epfl.ch>
2018-03-02 09:15:45 +0100

    by default, define repo within lifev_config.sh

--- clone_lifev.sh
+++ clone_lifev.sh
@@ -5,15 +5,22 @@
 # Andrea Bartezzaghi, 19-Feb-2018
 #
 
-if [ "$#" != "1" ]; then
+if [ "z$1" == "z-h" ]; then
+
     echo "Usage:"
     echo "./clone_lifev.sh <repo>"
+    echo "if <repo> is not given. it is taken from lifev_config.sh"
     echo
     echo "Example: ./clone_lifev.sh lifev-release"
     exit 1
 fi
 
-REPO="$1"
+if [ "$#" != "1" ]; then
+    source lifev_config.sh
+    REPO=$LIFEV
+else
+    REPO="$1"
+fi
 
 git clone git@bitbucket.org:lifev-dev/$REPO $REPO
 
