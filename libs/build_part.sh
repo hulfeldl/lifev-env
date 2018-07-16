1b7949766b4f407e5f9f2495d458b0b287a0a3dc
Matthias Lange <Matthias.lange@polimi.it>
2018-07-16 13:01:24 +0200

    Try to splite the build process in different steps to get below 2 hours. Unsure wheather bitbucket runs on docker process or for each step a new.

---
+++ libs/build_part.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Build all libraries at once
+#
+# Andrea Bartezzaghi, 15-Nov-2017
+#
+# Modified: 
+# Matthias Lange, 27-Jun-2017
+# Added comand line interface argument for config file.
+
+set -e
+
+
+# Remove from the list the packages that you already installed
+PACKS="openblas boost metis parmetis hdf5 suitesparse trilinos"
+
+for p in $PACKS; do
+    echo building $p, outout to $p.out
+    nohup ./build_$p.sh $1 > $p.out || ( echo error compiling $p && exit 1)
+    echo built $p
+done
